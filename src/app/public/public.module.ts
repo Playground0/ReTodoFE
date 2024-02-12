501eef93051d4f43fd4dd0a7906906528646004f
Playground0 <emailplayg1@gmail.com>
2024-02-13 00:23:15 +0530

    create lazy loading and assign routes for basic app

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DemoComponent } from './demo/demo.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    DemoComponent,
+    AboutusComponent
+  ],
   imports: [
     CommonModule,
     PublicRoutingModule
